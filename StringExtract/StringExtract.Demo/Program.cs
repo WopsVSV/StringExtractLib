3a410f98532545d9452b8d58aa32d6e9af3bd424
Sorin Valentin Vijoaica <wops@null.net>
2017-08-14 10:39:45 +0300

    Basic structure added.

---
+++ StringExtract/StringExtract.Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using StringExtract.Library;
+
+namespace StringExtract.Demo
+{
+    /// <summary>
+    /// Startup object for the demo of StringExtract.
+    /// </summary>
+    public class Program
+    {
+        // The dummy program name
+        private const string DUMMY_PATH = "StringExtract.Dummy.exe";
+
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="args">The arguments passed through the command line.</param>
+        public static void Main(string[] args)
+        {
+            // Creates a new extractor
+            var extractor = new Extractor();
+
+            // Extracts the strings
+            extractor.Extract(DUMMY_PATH);
+
+            // Prints out the output
+            foreach (var value in extractor.Strings)
+                Console.WriteLine(value);
+
+            // Await input before exit
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ StringExtract/StringExtract.Library/Extractor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StringExtract.Library
+{
+    /// <summary>
+    /// The extractor class, which contains the core logic of the program
+    /// </summary>
+    public class Extractor
+    {
+        // List of the extracted strings
+        public List<string> Strings;
+
+        /// <summary>
+        /// The constructor for the extractor
+        /// </summary>
+        public Extractor()
+        {
+            Strings = new List<string>();
+        }
+
+        /// <summary>
+        /// The main method that extracts the strings
+        /// </summary>
+        public void Extract(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    ParseStream(stream);
+
+                    stream.Flush();
+                }
+            }
+            else
+            {
+                throw new IOException($"File does not exist: {filePath}");
+            }
+        }
+
+        /// <summary>
+        /// Parses the input stream
+        /// </summary>
+        public void ParseStream(FileStream stream)
+        {
+            const int BUFFER_SIZE = 32768;
+
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
+            do
+            {
+                bytesRead = stream.Read(buffer, 0, BUFFER_SIZE);
+                if (bytesRead > 0)
+                {
+                    ProcessBuffer(buffer);
+                }
+
+            } while (bytesRead == BUFFER_SIZE);
+        }
+
+        /// <summary>
+        /// Processes a buffer
+        /// </summary>
+        /// <param name="buffer"></param>
+        public void ProcessBuffer(byte[] buffer)
+        {
+            
+        }
+    }
+}
