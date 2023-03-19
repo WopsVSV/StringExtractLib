3de5c12792a785fc172f23e9f9c99e496c15fe4a
Sorin Vijoaica <sorin.vijoaica@gmail.com>
2023-03-19 15:02:32 +0200

    Add xmldoc

--- src/StringExtractLib.Memory/MemorySource.cs
+++ src/StringExtractLib.Memory/MemorySource.cs
@@ -2,14 +2,33 @@
 
 namespace StringExtractLib
 {
+    /// <summary>
+    /// Represents a region of process memory with a fixed size.
+    /// </summary>
     public class MemorySource
     {
+        /// <summary>
+        /// Handle of the process.
+        /// </summary>
         public IntPtr Handle { get; private set; }
 
+        /// <summary>
+        /// Base address of the memory region.
+        /// </summary>
         public int Address { get; private set; }
 
+        /// <summary>
+        /// Length, in bytes, of the memory region.
+        /// </summary>
         public int Length { get; private set; }
 
+        /// <summary>
+        /// Creates a new memory region representation from given parameters.
+        /// </summary>
+        /// <param name="handle">Handle of the process.</param>
+        /// <param name="address">Base address of the memory region.</param>
+        /// <param name="length">Length, in bytes, of the memory region.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Memory address or length is less than 0.</exception>
         public MemorySource(IntPtr handle, int address, int length)
         {
             if (address < 0)
--- src/StringExtractLib.Memory/MemoryStringReader.cs
+++ src/StringExtractLib.Memory/MemoryStringReader.cs
@@ -4,32 +4,63 @@ using System.Runtime.InteropServices;
 
 namespace StringExtractLib
 {
+    /// <summary>
+    /// An <see cref="IStringReader"/> implementation used to extract strings from a memory region.
+    /// </summary>
     public class MemoryStringReader : IStringReader
     {
         [DllImport("kernel32.dll")]
         private static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, out int lpNumberOfBytesRead);
 
+        /// <summary>
+        /// The memory region to extract strings from.
+        /// </summary>
         public MemorySource Source { get; private set; }
 
+        /// <summary>
+        /// The extraction options which the reader will follow.
+        /// </summary>
         public StringReaderOptions Options { get; private set; }
 
+        /// <summary>
+        /// Creates a new <see cref="MemoryStringReader"/> for a given memory source
+        /// using default string reader options.
+        /// </summary>
+        /// <param name="source">The target memory source.</param>
         public MemoryStringReader(MemorySource source)
         {
             Source = source;
             Options = new StringReaderOptions();
         }
 
+        /// <summary>
+        /// Creates a new <see cref="MemoryStringReader"/> for a given memory source
+        /// using specific string reader options.
+        /// </summary>
+        /// <param name="source">The target memory source.</param>
+        /// <param name="options">The string reader options.</param>
         public MemoryStringReader(MemorySource source, StringReaderOptions options)
         {
             Source = source;
             Options = options;
         }
 
+        /// <summary>
+        /// Reads and returns all strings from the target memory region using the string reader
+        /// options set up while constructing the <see cref="MemoryStringReader"/>.
+        /// </summary>
+        /// <returns>A list of all strings, filtered by the options.</returns>
         public IList<string> ReadAll()
         {
             return ReadAll(Options);
         }
 
+        /// <summary>
+        /// Reads and returns all strings from the target memory region using the string reader
+        /// options passed as a parameter.
+        /// Note: Reading from memory using <see cref="MemoryStringReader"/> is only possible on Windows platforms.
+        /// </summary>
+        /// <returns>A list of all strings, filtered by the options.</returns>
         public IList<string> ReadAll(StringReaderOptions options)
         {
             if (options == null)
